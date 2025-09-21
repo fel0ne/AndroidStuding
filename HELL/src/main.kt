a475c8ad932c9e0200d473c7e17e813fe88f671d
PavelVOLK1929 <pavelvolevatch@yandex.ru>
2025-09-21 17:15:50 +0700

    update branch

--- HELL/src/main.kt
+++ HELL/src/main.kt
@@ -7,12 +7,49 @@ class Human(t_name: String, t_age: Int, t_current_speed: Int){
     var current_speed = t_current_speed
     var x: Double = 0.0
     var y: Double = 0.0
+
     fun move(){
         val r: Double = Random.nextDouble(0.0,2*Math.PI)
         x += current_speed * sin(r)
         y += current_speed * cos(r)
-        println("$name перешел в ($x ;$y)")
+        //println("$name перешел в ($x ;$y)")
     }
+
+    //---------geters--------
+    fun getName(): String{
+        return(name)
+    }
+    fun getAge(): Int{
+        return(age)
+    }
+    fun getCurrentSpeed(): Int{
+        return(current_speed)
+    }
+    fun getX(): Double{
+        return(x)
+    }
+    fun getY(): Double{
+        return(y)
+    }
+
+
+    //-----setters-----
+    fun setName(p: String){
+        name = p
+    }
+    fun setAge(p: Int){
+        age = p
+    }
+    fun setCurrent(p: Int){
+        current_speed = p
+    }
+    fun setX(p: Double){
+        x = p
+    }
+    fun setY(p: Double){
+        y = p
+    }
+
 }
 
 fun main(){
@@ -20,7 +57,12 @@ fun main(){
     val people = arrayOfNulls<Human>(7)
 
     for (i in 0..6){
-        val newHuman = Human(t_name = "человек номер ${i+1}", t_age = Random.nextInt(1,65), t_current_speed = Random.nextInt(1,5))
+        val newHuman: Human
+        newHuman.setName("человек $i")
+        newHuman.setAge(Random.nextInt(0,1))
+        newHuman.setCurrent(Random.nextInt(0,9))
+
+
         people[i] = newHuman
     }
     println("созданные люди:")
